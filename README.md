1771bd5425353c4a0eb9d6dc17aa9d0ec49896df
AlvinLee66 <ewqaz001@gmail.com>
2022-04-02 13:01:43 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Restaurant List
+<br>
+:sparkles: Record your favorite restaurant.  
+
+## Features  
+* Shows all restaurants on homepage.  
+* Click image for detailed info on restaurant.  
+* You can find the specific restaurant by searching keywords(restaurant name or category).
+
+## Installation
+* Install the Node.js and npm according to your OS(MacOS or Windows).
+* Download the project to your local machine.
+* Get into your project folder by Terminal and enter `npm install`.
+
+## How to use
+* After installation, enter `node app.js` to execute the localhost.
+* Open the browser, and enter the URL `http://localhost:3000`.
+
+## Build with
+* Node.js @14.16.0
+* Express.js @4.16.4
+* express-handlebars @3.0.0
+* Bootstrap @5.1.3
+* Font-awesome @5.8.1
---
+++ app.js
@@ -0,0 +1,39 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurantId = req.params.restaurant_id
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === restaurantId)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    const combineNameAndCategory = restaurant.name + restaurant.category
+    return combineNameAndCategory.toLowerCase().includes(keyword.toLowerCase())
+  })
+  
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
